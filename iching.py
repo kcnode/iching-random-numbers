660462a3cbe0526269da71ee070117c8f214a4ed
KC Create <kcnode123@gmail.com>
2025-02-12 09:44:23 +0800

    Add files via upload

--- iching.py
+++ iching.py
@@ -1,10 +1,4 @@
-echo "# iching-random-numbers" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/kcnode/iching-random-numbers.git
-git push -u origin main
+
 
 
 import random
